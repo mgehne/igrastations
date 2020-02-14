e2adc88c62e77fab131a22cb44f8e1a56b68ee72
mgehne <mgehne@gmail.com>
2020-02-14 14:45:34 -0700

    changed types in getstationdates

--- igrastations/getstationdates.py
+++ igrastations/getstationdates.py
@@ -24,19 +24,19 @@ def getstationdates(filename):
         nlines += 1
         if line[0] == '#':
             toks = line.split()
-            years.append(toks[1])
-            mons.append(toks[2])
-            days.append(toks[3])
-            hours.append(toks[4])
+            years.append(int(toks[1]))
+            mons.append(int(toks[2]))
+            days.append(int(toks[3]))
+            hours.append(int(toks[4]))
 
-            lats.append(toks[8])
-            lons.append(toks[9])
+            lats.append(float(toks[8]))
+            lons.append(float(toks[9]))
 
-    years = np.array(years)
-    mons = np.array(mons)
-    days = np.array(days)
-    hours = np.array(hours)
-    lats = np.array(lats)
-    lons = np.array(lons)
+        years = np.array(years)
+        mons = np.array(mons)
+        days = np.array(days)
+        hours = np.array(hours)
+        lats = np.array(lats) / 10000
+        lons = np.array(lons) / 10000
 
     return years, mons, days, hours, lats, lons
